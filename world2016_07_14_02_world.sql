bfe156d36eea35ae1a28d03ef7c55db036a713a5
Nibelar <Nibelar@users.noreply.github.com>
2016-07-14 21:55:17 +1000

    Create world2016_07_14_02_world.sql
    loot for item 41426

---
+++ world2016_07_14_02_world.sql
@@ -0,0 +1,4 @@
+INSERT INTO `item_loot_template` VALUES ('41426', '41427', '0', '99', '0', '1', '0', '5', '5', null);
+INSERT INTO `item_loot_template` VALUES ('41426', '40211', '0', '99', '0', '1', '0', '5', '5', null);
+INSERT INTO `item_loot_template` VALUES ('41426', '33447', '0', '98', '0', '1', '0', '5', '5', null);
+INSERT INTO `item_loot_template` VALUES ('41426', '33448', '0', '98', '0', '1', '0', '5', '5', null);
